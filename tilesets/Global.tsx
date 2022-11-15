eba3335c5c3b00c43d42eb085d813f69950920f3
SW3xyz <alain@studioweb3.xyz>
2022-11-15 03:53:44 +0100

    Create Global.tsx

---
+++ tilesets/Global.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Brainsonic_Workadventure_Global" tilewidth="32" tileheight="32" tilecount="8649" columns="93">
+ <image source="../../test/tilesets/Brainsonic_Workadventure_Global.png" trans="ff00ff" width="3000" height="3000"/>
+</tileset>
